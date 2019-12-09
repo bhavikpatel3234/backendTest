2226156e79a93b5cbbfe4a614788d99baa6a546c
Bhavik Patel <bhavikpatel3234@yahoo.com>
2019-12-08 23:37:50 -0600

    Optimizing import of exception

--- src/main/java/io/golo/backendtest/service/impl/HTTPErrorCodeServiceImpl.java
+++ src/main/java/io/golo/backendtest/service/impl/HTTPErrorCodeServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResponseErrorHandler;
 
-import java.io.*;
+import java.io.IOException;
 
 @Service
 public class HTTPErrorCodeServiceImpl implements ResponseErrorHandler {
